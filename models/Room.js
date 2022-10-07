05350be3da66b3d435158850f5a8dd372210abc7
Koulinski <emil.k.koulinski@gmail.com>
2022-10-07 13:14:52 +0400

    Added Facility model and controller

--- models/Room.js
+++ models/Room.js
@@ -1,5 +1,5 @@
 
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 
 const roomSchema = new Schema({
@@ -8,7 +8,8 @@ const roomSchema = new Schema({
     city: { type: String, required: true },
     beds: { type: Number, required: true, min: 1, max: 8 },
     price: { type: Number, required: true, min: 0.01, max: 999999 },
-    imgUrl: { type: String }
+    imgUrl: { type: String },
+    facilities: { type: [Types.ObjectId], default: [], ref: 'Facility' }
 });
 
 const Room = model('Room', roomSchema);
